701d02656627ed7b0c2e8b18a8f239d3fe836bd4
guimarczewski <guimarczewski@gmail.com>
2024-01-16 21:47:32 +0000

    adicionado novas verificacoes

--- app/schema.py
+++ app/schema.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt
+from typing import Optional
 
 class ProdutosSchema(BaseModel):
     id: int
     nome: str
-    descricao: str
-    preco: float
+    descricao: Optional[str] = None
+    preco: PositiveInt
