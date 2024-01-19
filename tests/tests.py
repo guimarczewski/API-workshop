88bfc86e79e85a549e16b6d2819c92178677b509
guimarczewski <guimarczewski@gmail.com>
2024-01-19 16:55:04 +0000

    change tests

--- tests/tests.py
+++ tests/tests.py
@@ -10,23 +10,6 @@ def test_ola_mundo():
     response = client.get("/")
     assert response.status_code == 200
 
-def test_ola_mundo_json():
-    response = client.get("/")
-    assert response.json() == {"Hello":"World"}
-
 def test_listar_produtos_status_code():
     response = client.get("/produtos")
     assert response.status_code == 200
-
-def test_tamanho_lista_produtos():
-    response = client.get("/produtos")
-    assert len(response.json()) == 3
-
-def teste_pega_um_produto():
-    response = client.get("/produtos/1")
-    assert response.json() == {
-        "id":1,
-        "nome":"Smartphone",
-        "descricao":"Um telefone inteligente",
-        "preco":1500.00
-    }
