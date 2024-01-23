28ad7aa5a05c71ef5c3224e0415417633f0de8ef
guimarczewski <guimarczewski@gmail.com>
2024-01-23 21:56:32 +0000

    update

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,19 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 
+from dotenv import load_dotenv
+
+load_dotenv()
+# load_dotenv(dotenv_path="API/.env-prod")
+
+db_user = os.getenv("POSTGRES_USER")
+db_password = os.getenv("POSTGRES_PASSWORD")
+db_name = os.getenv("POSTGRES_DB")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+
+print(db_port)
+
 client = TestClient(app)
 
 def test_ola_mundo():
